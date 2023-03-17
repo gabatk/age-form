837511466f25d863947b85daddae02e424989945
gabatk <gabriela.trybuszek@gmail.com>
2023-03-17 12:00:24 +0100

    User item element prepared.

---
+++ src/component/UserItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './UserItem.css';
+
+const UserItem = () => {
+	return (
+		<li className='user__item'>
+			<span className='user__item--span'>Izik</span> (2 years old)
+		</li>
+	);
+};
+
+export default UserItem;
