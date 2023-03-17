05a46b624f8137aaca0a518f3e413f7bb37bfeb9
gabatk <gabriela.trybuszek@gmail.com>
2023-03-17 11:14:24 +0100

    New CSS styles implemented on button.

---
+++ src/component/AppButton.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './AppButton.css'
+
+const AppButton = () => {
+    return (
+        <button type='submit' className="btn">Add User</button>
+    )
+}
+
+export default AppButton;
--- src/component/FormPanel.js
+++ src/component/FormPanel.js
@@ -1,21 +1,25 @@
 import React from 'react';
-import AppHeader from './AppHeader'
+import AppHeader from './AppHeader';
+import AppButton from './AppButton';
 import './FormPanel.css';
 
 const FormPanel = () => {
-
-    return (
-        <div className='box'>
-            <AppHeader />
-        <form>
-            <label for='username'>Username:</label>
-            <input type={'text'} name='username'></input>
-            <label for='age'>Age (Years):</label>
-            <input type={'number'} name='age'></input>
-        </form>
-        <button type='submit'>Add User</button>
-        </div>
-    )
+	return (
+		<div className='box'>
+			<AppHeader />
+			<form className='form__box'>
+				<label for='username' className='form__label'>
+					Username:
+				</label>
+				<input type={'text'} name='username' className='form__input'></input>
+				<label for='age' className='form__label'>
+					Age (Years):
+				</label>
+				<input type={'number'} name='age' className='form__input'></input>
+			</form>
+			<AppButton />
+		</div>
+	);
 };
 
 export default FormPanel;
